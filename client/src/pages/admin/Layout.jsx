43cfddf41b39b5819005d760ee15f6a42f0711c7
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-07-20 17:31:18 +0530

    implemented the add blog feature

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = () => {
   return (
     <>
     <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
-    <img onClick={navigate("/")} src={assets.logo} className='w-32 sm:w-40 cursor-pointer' alt="" />
+    <img onClick={()=>navigate("/")} src={assets.logo} className='w-32 sm:w-40 cursor-pointer' alt="" />
     <button className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer' onClick={()=>logout()}>Logout</button>
     </div>
     <div className='flex h-[calc(100vh-70px)]'>
